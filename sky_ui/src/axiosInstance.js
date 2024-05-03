b76e7d7c5b47b2ce710febedf33908d2ea3fd272
Ritik Singh <singhritik2711@gmail.com>
2024-05-04 00:16:39 +0530

    final changes

--- sky_ui/src/axiosInstance.js
+++ sky_ui/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: `http://3.25.197.125:8000`
+    baseURL: `http://13.239.114.129:8000` 
     // baseURL: `http://localhost:8000`
 });
 
