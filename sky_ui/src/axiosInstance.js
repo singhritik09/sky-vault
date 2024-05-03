de4cf126801efbdb2cf6f0717fe27f4cf6120267
Ritik Singh <singhritik2711@gmail.com>
2024-05-03 13:48:24 +0530

    changes

--- sky_ui/src/axiosInstance.js
+++ sky_ui/src/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const axios1= axios.create({
+const axiosInstance = axios.create({
     withCredentials: true,
-    // baseURL: `http://3.25.197.125:3000`
-    baseURL: `http://localhost:8000`
-})
+    baseURL: `http://3.25.197.125:3000`
+    // baseURL: `http://localhost:8000`
+});
 
-export default axios1;
+export default axiosInstance
